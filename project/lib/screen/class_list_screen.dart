39b6af4af817a6e9571fd9344095e8fdf8b3b3b8
CaoThaiTuan <103022432+ThaiTuan91@users.noreply.github.com>
2023-01-31 14:44:55 +0700

    update

---
+++ project/lib/screen/class_list_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class ClassListScreen extends StatefulWidget {
+  const ClassListScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ClassListScreen> createState() => _ClassListScreenState();
+}
+
+class _ClassListScreenState extends State<ClassListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+      ),
+    );
+  }
+}
