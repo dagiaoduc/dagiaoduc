e7ffe270ae913beb85c9aca8cb98e5ed37715945
CaoThaiTuan <103022432+ThaiTuan91@users.noreply.github.com>
2023-03-07 14:23:23 +0700

    update

--- project/lib/config/config.dart
+++ project/lib/config/config.dart
@@ -1,14 +1,16 @@
-const String BASE_URL = 'http://192.168.0.104';
-const String BASE_URL_REST = 'http://192.168.0.104';
+const String BASE_URL = 'https://sample-gozic.000webhostapp.com';
+const String BASE_URL_REST = 'https://sample-gozic.000webhostapp.com';
 
 const String API_CLASS = BASE_URL + "/api/v1/classes";
 const String API_CLASS_FIND= BASE_URL + "/api/v1/classes/";
 
+const String API_REGSITER = BASE_URL + "/api/user/regsiter";
 
-const String  API_USERS_LIST = BASE_URL + "/api/v1/users";
 
-const String API_STUDENTS_LIST = BASE_URL + "/api/v1/students";
-const String API_STUDENTS_FIND = BASE_URL + "/api/v1/students/";
+const String API_USERS_INFOR = BASE_URL + " api/user/infor";
+
+
+const String API_STUDENTS_LIST = BASE_URL + "/api/class/student-list";
 
 
 
