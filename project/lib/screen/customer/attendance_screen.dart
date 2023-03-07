65bf92931f5dd9c01a8332dba6dcf27d3bb8cfb2
NguyenLong19 <94092345+NguyenLong19@users.noreply.github.com>
2023-03-07 15:52:35 +0700

    update

--- project/lib/screen/customer/attendance_screen.dart
+++ project/lib/screen/customer/attendance_screen.dart
@@ -201,7 +201,7 @@ class AttendanceScreen extends MainState{
                             width:x*0.15,
                             alignment: Alignment.center ,
                             child: Radio(
-                              value: 0,
+                              value: 1,
                               groupValue: _value,
                               onChanged: (value){
                                 setState(() {
@@ -214,7 +214,7 @@ class AttendanceScreen extends MainState{
                             width:x*0.15,
                             alignment: Alignment.center ,
                             child: Radio(
-                              value: 1,
+                              value: 2,
                               groupValue: _value,
                               onChanged: (value){
                                 setState(() {
